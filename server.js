0771a69a43f80edf7e9080378679d16b26102e7d
chehak <gchehak18@gmail.com>
2024-07-05 23:40:19 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+console.log("radhe radhe");
+
+var os = require("os");
+var fs = require("fs");
+var _ = require("lodash");
+var fnImport = require("./add.js");
+console.log(os.userInfo().username);
+fs.appendFile(
+  "firstMessage.txt",
+  "Hello " + os.userInfo().username + "!\n",
+  () => console.log("file is created")
+);
+
+console.log(fnImport.add(200, 300));
+
+const arr = [1, 1, 2, 3, 4, 3, 4, "cheeku", "chehak"];
+
+console.log(_.uniq(arr));
