b5082015f4522e6a2f88e819e2e219ba166a2d68
Chehak Gupta <gchehak18@gmail.com>
2024-07-19 16:17:51 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,11 @@
-
-
 const mongoose = require("mongoose");
-const mongoUrl ='mongodb://localhost:27017/hotels';
-
-mongoose.connect(mongoUrl , {})
-
+require('dotenv').config();
+// const mongoUrl = process.env.MONGODB_LOCAL_SERVER_URL;
+const mongoUrl = "mongodb+srv://gchehak18:Chehak1999@cluster0.xfhqjeh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(mongoUrl ,  {})
 const db = mongoose.connection;
-
 db.on('connected',()=>{
+    console.log(mongoUrl,"momgourl");
     console.log("connected");
 })
 
