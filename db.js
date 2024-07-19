f995741865ebdea457055e2fd8be52fd3b4fa652
Chehak Gupta <gchehak18@gmail.com>
2024-07-19 16:57:29 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
-const mongoUrl = process.env.MONGODB_LOCAL_SERVER_URL;
+const mongoUrl = process.env.MONGODB_LIVE_SERVER_URL;
 mongoose.connect(mongoUrl ,  {})
 const db = mongoose.connection;
 db.on('connected',()=>{
